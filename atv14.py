fbdddf2c0025225a3011389f32b54bfc1bf9fd64
João Victor <136256603+SnowHiroki@users.noreply.github.com>
2026-07-23 19:22:27 -0300

    Add files via upload

---
+++ atv14.py
@@ -0,0 +1,6 @@
+#Convertendo Temperatura Celsius para Fahrenheit
+
+C = float(input('Informe a temperatura em Celsius:'))
+F = C * 1.8 + 32
+print(f'{C} Celsius corresponde a {F:.01f} Fahrenheit')
+
